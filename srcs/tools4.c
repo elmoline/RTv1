b14a91f9371da7b3d5805b24b24fef25cb48382e
wael-mos <welmossannif@lilo.org>
2019-10-23 14:24:55 +0200

    add error msg

--- srcs/tools4.c
+++ srcs/tools4.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tools4.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: evogel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: wael-mos <wael-mos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 14:03:04 by evogel            #+#    #+#             */
-/*   Updated: 2019/10/22 15:54:02 by evogel           ###   ########.fr       */
+/*   Updated: 2019/10/23 14:23:40 by wael-mos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,14 @@ void	add_color(t_col *ori, t_col *add, float amount)
 void	errormsg(int msg)
 {
 	if (msg == 0)
-	{
 		ft_printf("Error: Wrong character in the scene file.\n");
-		exit(-1);
-	}
 	else if (msg == 1)
-	{
 		ft_printf("Error: Wrong format in the scene file.\n");
-		exit(-1);
-	}
 	else if (msg == 2)
-	{
 		ft_printf("Error: Failed malloc.\n");
-		exit(-1);
-	}
+	else if (msg == 3)
+		ft_printf("Usage: ./rtv1 [file.scene]\n");
+	exit(-1);
 }
 
 void	freetab(char **tab)
